e0aeca6df0df5897c987b5b7d3644dba27eb403c
Sunil Chauhan <sunil121ml@gmail.com>
2023-03-05 12:53:17 +0530

    adding /scan route and qr code genrator

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const qrCode = require("qrcode");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3100;
@@ -10,8 +11,19 @@ app.set("views", path.join(__dirname, "view"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/scan", (req, res) => {
+  const inputText = req.body.qrData;
+  qrCode.toDataURL(inputText, (err, src) => {
+    res.render('scan', {
+      qrSrc: src
+    });
+  })
 });
 
 app.listen(PORT, () => {
